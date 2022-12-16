121691d4d2ac9096d4b935d295ad0c45ce09cc54
Deepak <deepakborania@users.noreply.github.com>
2022-12-16 00:16:24 +0000

    tun_tap working

---
+++ trust/run.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+cargo b --release
+sudo setcap cap_net_admin=eip target/release/trust
+./target/release/trust &
+pid=$!
+sudo ip addr add 192.168.0.1/24 dev tun0
+sudo ip link set up dev tun0
+wait $pid
