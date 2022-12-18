539c1e9c4c56849b8d9cdc70111361c0e70ed94b
Deepak <deepakborania@users.noreply.github.com>
2022-12-18 23:38:03 +0000

    yolo

--- trust/src/main.rs
+++ trust/src/main.rs
@@ -11,44 +11,50 @@ struct Quad {
 }
 
 fn main() -> io::Result<()> {
-    let mut connections: HashMap<Quad, tcp::State> = Default::default();
-    let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
+    let mut connections: HashMap<Quad, tcp::Connection> = Default::default();
+    let mut nic = tun_tap::Iface::without_packet_info("tun0", tun_tap::Mode::Tun)?;
     let mut buf = [0u8; 1504];
     loop {
         let nbytes = nic.recv(&mut buf[..])?;
-        let _eth_flags = u16::from_be_bytes([buf[0], buf[1]]);
-        let eth_proto = u16::from_be_bytes([buf[2], buf[3]]);
-        if eth_proto != 0x0800 {
-            // Not an ipv4 packet
-            continue;
-        }
+        // let _eth_flags = u16::from_be_bytes([buf[0], buf[1]]);
+        // let eth_proto = u16::from_be_bytes([buf[2], buf[3]]);
+        // if eth_proto != 0x0800 {
+        //     // Not an ipv4 packet
+        //     continue;
+        // }
 
-        match etherparse::Ipv4HeaderSlice::from_slice(&buf[4..nbytes]) {
-            Ok(p) => {
-                let src = p.source_addr();
-                let dst = p.destination_addr();
-                let proto = p.protocol();
-                if proto != 0x06 {
+        match etherparse::Ipv4HeaderSlice::from_slice(&buf[..nbytes]) {
+            Ok(iph) => {
+                let src = iph.source_addr();
+                let dst = iph.destination_addr();
+                if iph.protocol() != 0x06 {
                     // not tcp
                     continue;
                 }
 
-                let ip_hdr_size = p.slice().len();
-                match etherparse::TcpHeaderSlice::from_slice(&buf[4 + p.slice().len()..]) {
-                    Ok(p) => {
-                        let data = 4 + ip_hdr_size+p.slice().len();
-                        connections.entry(Quad{
-                            src:(src,p.source_port()),
-                            dst:(dst, p.destination_port()),
-                        }).or_default().on_packet(p, );
-
-                        eprintln!(
-                            "{} → {}  {}b of tcp to port {}",
-                            src,
-                            dst,
-                            p.slice().len(),
-                            p.destination_port(),
-                        );
+                match etherparse::TcpHeaderSlice::from_slice(&buf[iph.slice().len()..nbytes]) {
+                    Ok(tcph) => {
+                        use std::collections::hash_map::Entry;
+                        let datai = iph.slice().len() + tcph.slice().len();
+                        match connections.entry(Quad {
+                            src: (src, tcph.source_port()),
+                            dst: (dst, tcph.destination_port()),
+                        }) {
+                            Entry::Occupied(mut c) => {
+                                c.get_mut()
+                                    .on_packet(&mut nic, iph, tcph, &buf[datai..nbytes])?;
+                            }
+                            Entry::Vacant(mut e) => {
+                                if let Some(c) = tcp::Connection::accept(
+                                    &mut nic,
+                                    iph,
+                                    tcph,
+                                    &buf[datai..nbytes],
+                                )? {
+                                    e.insert(c);
+                                }
+                            }
+                        }
                     }
                     Err(e) => {
                         eprintln!("Ignoring weird TCP packet {:?}", e);
--- trust/src/tcp.rs
+++ trust/src/tcp.rs
@@ -1,1 +1,144 @@
-pub struct State {}
+use std::io;
+
+enum State {
+    Closed,
+    Listen,
+    SynRcvd,
+    // Estab,
+}
+
+pub struct Connection {
+    state: State,
+    send: SendSequenceSpace,
+    recv: RecvSequenceSpace,
+}
+
+struct SendSequenceSpace {
+    /// Send unacknowledged
+    una: u32,
+    /// Send next
+    nxt: u32,
+    /// Send window
+    wnd: u16,
+    /// Send urgent pointer
+    up: bool,
+    /// segment sequence number for last window update
+    wl1: usize,
+    /// segment acknowledge number for last window update
+    wl2: usize,
+    /// Initial send sequence number
+    iss: u32,
+}
+
+struct RecvSequenceSpace {
+    /// Receive next
+    nxt: u32,
+    /// Receive window
+    wnd: u16,
+    /// Receive urgent pointer
+    up: bool,
+    /// Initial Receive sequence number
+    irs: u32,
+}
+
+// impl Default for Connection {
+//     fn default() -> Self {
+//         // State::Closed
+//         Connection {
+//             state: State::Listen,
+//         }
+//     }
+// }
+
+impl Connection {
+    pub fn accept<'a>(
+        nic: &mut tun_tap::Iface,
+        iph: etherparse::Ipv4HeaderSlice<'a>,
+        tcph: etherparse::TcpHeaderSlice<'a>,
+        data: &'a [u8],
+    ) -> io::Result<Option<Self>> {
+        let mut buf = [0u8; 1500];
+
+        if !tcph.syn() {
+            // only expected SYN packet
+            return Ok(None);
+        }
+
+        let iss = 0;
+        let mut c = Connection {
+            state: State::SynRcvd,
+            send: SendSequenceSpace {
+                iss,
+                una: iss,
+                nxt: iss + 1,
+                wnd: 10,
+                up: false,
+                wl1: 0,
+                wl2: 0,
+            },
+            recv: RecvSequenceSpace {
+                nxt: tcph.sequence_number() + 1,
+                wnd: tcph.window_size(),
+                irs: tcph.sequence_number(),
+                up: false,
+            },
+        };
+
+        //keep track of sender info
+
+        //decide on the stuff we are sending them
+
+        let mut syn_ack = etherparse::TcpHeader::new(
+            tcph.destination_port(),
+            tcph.source_port(),
+            c.send.iss,
+            c.send.wnd,
+        );
+        syn_ack.acknowledgment_number = c.recv.nxt;
+        syn_ack.syn = true;
+        syn_ack.ack = true;
+
+        let mut ip = etherparse::Ipv4Header::new(
+            syn_ack.header_len(),
+            64,
+            etherparse::IpTrafficClass::Tcp,
+            [
+                iph.destination()[0],
+                iph.destination()[1],
+                iph.destination()[2],
+                iph.destination()[3],
+            ],
+            [
+                iph.source()[0],
+                iph.source()[1],
+                iph.source()[2],
+                iph.source()[3],
+            ],
+        );
+
+        // syn_ack.checksum = syn_ack
+        //     .calc_checksum_ipv4(&ip, &[])
+        //     .expect("failed to compute checksum");
+
+        let unwritten = {
+            let mut unwritten = &mut buf[..];
+            ip.write(&mut unwritten);
+            syn_ack.write(&mut unwritten);
+            unwritten.len()
+        };
+        nic.send(&buf[..(buf.len() - unwritten)])?;
+        Ok(Some(c))
+    }
+
+    pub fn on_packet<'a>(
+        &mut self,
+        nic: &mut tun_tap::Iface,
+        iph: etherparse::Ipv4HeaderSlice<'a>,
+        tcph: etherparse::TcpHeaderSlice<'a>,
+        data: &'a [u8],
+    ) -> io::Result<()> {
+        match self.state {
+            
+        }
+    }
+}
