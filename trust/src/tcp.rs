07300d49e7fc301da0efb2c8ce15b0bdabfcdea1
Deepak <deepakborania@users.noreply.github.com>
2022-12-20 17:46:35 +0000

    commit

--- trust/src/tcp.rs
+++ trust/src/tcp.rs
@@ -1,16 +1,17 @@
 use std::io;
 
 enum State {
-    Closed,
-    Listen,
+    // Closed,
+    // Listen,
     SynRcvd,
-    // Estab,
+    Estab,
 }
 
 pub struct Connection {
     state: State,
     send: SendSequenceSpace,
     recv: RecvSequenceSpace,
+    ip: etherparse::Ipv4Header,
 }
 
 struct SendSequenceSpace {
@@ -82,6 +83,23 @@ impl Connection {
                 irs: tcph.sequence_number(),
                 up: false,
             },
+            ip: etherparse::Ipv4Header::new(
+                0,
+                64,
+                etherparse::IpTrafficClass::Tcp,
+                [
+                    iph.destination()[0],
+                    iph.destination()[1],
+                    iph.destination()[2],
+                    iph.destination()[3],
+                ],
+                [
+                    iph.source()[0],
+                    iph.source()[1],
+                    iph.source()[2],
+                    iph.source()[3],
+                ],
+            ),
         };
 
         //keep track of sender info
@@ -97,32 +115,14 @@ impl Connection {
         syn_ack.acknowledgment_number = c.recv.nxt;
         syn_ack.syn = true;
         syn_ack.ack = true;
-
-        let mut ip = etherparse::Ipv4Header::new(
-            syn_ack.header_len(),
-            64,
-            etherparse::IpTrafficClass::Tcp,
-            [
-                iph.destination()[0],
-                iph.destination()[1],
-                iph.destination()[2],
-                iph.destination()[3],
-            ],
-            [
-                iph.source()[0],
-                iph.source()[1],
-                iph.source()[2],
-                iph.source()[3],
-            ],
-        );
-
+        c.ip.set_payload_len(syn_ack.header_len() as usize + 0);
         // syn_ack.checksum = syn_ack
-        //     .calc_checksum_ipv4(&ip, &[])
+        //     .calc_checksum_ipv4(&c.ip, &[])
         //     .expect("failed to compute checksum");
 
         let unwritten = {
             let mut unwritten = &mut buf[..];
-            ip.write(&mut unwritten);
+            c.ip.write(&mut unwritten);
             syn_ack.write(&mut unwritten);
             unwritten.len()
         };
@@ -137,8 +137,52 @@ impl Connection {
         tcph: etherparse::TcpHeaderSlice<'a>,
         data: &'a [u8],
     ) -> io::Result<()> {
+        // Acceptable ACK check
+        // SND>UNA < SEG>ACK =< SND.NXT
+        let ackn = tcph.acknowledgment_number();
+        if self.send.una < ackn {
+            // check is violated iff n is between u and a
+            if self.send.nxt >= self.send.una && self.send.nxt < ackn {
+                return Ok(());
+            }
+        } else {
+            //check is okay iff n is between u and a
+            if self.send.nxt >= ackn && self.send.nxt < self.send.una {
+            } else {
+                return Ok(());
+            }
+        }
+
+        // valid segment check
+
         match self.state {
-            
+            State::SynRcvd => {}
+            State::Estab => {
+                unimplemented!();
+            }
+        }
+
+        Ok(())
+    }
+}
+
+fn is_between_wrapped(start: usize, x: usize, end: usize) -> bool {
+    use std::cmp::{Ord, Ordering};
+    match start.cmp(x) {
+        Ordering::Equal => return false,
+        Ordering::Less => {
+            // check is violated iff end is between start and x
+            if end >= start && end <= x {
+                return false;
+            }
+        },
+        Ordering::Greater => {
+            //check is okay iff end is between start and x
+            if end >= x && end < start {
+            } else {
+                return false;
+            }
         }
     }
+    true
 }
